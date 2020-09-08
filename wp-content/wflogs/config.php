97ebe8a9e4b5542f7a0fe0d7cf4567fe9bd9b277
NicolasLOrtiz <contato.nicolol@gmail.com>
2020-09-08 08:12:41 -0300

    pluginsThemes

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1599849468;s:7:"authKey";s:64:"Vvb w<zhRp%7,ORfj6G-+gV+)p@~e`0<nyZn@B+}.K}#.NF*i;=9C4sEaLtw5Uh@";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:3696;}
