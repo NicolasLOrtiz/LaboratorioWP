11ecfcd8757701ebaa9dc8e84c36eed52935dcfe
NicolasLOrtiz <contato.nicolol@gmail.com>
2020-09-30 14:08:36 -0300

    ChildThemes-P1

--- wp-content/themes/teck/functions.php
+++ wp-content/themes/teck/functions.php
@@ -9,4 +9,6 @@ function my_theme_enqueue_styles() {
     );
     wp_enqueue_style( 'child-style', get_stylesheet_uri() . '/style.css',
         array( $parenthandle ),
-        $theme->get('Version') // this only works if you have Version in the style he
+        $theme->get('Version') // this only works if you have Version in the style header
+    );
+}
